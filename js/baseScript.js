bd8d2d97cf372c66c6e6df6bbac213e8a6a3ecd0
Ingredientsof <dpens0705@gmail.com>
2025-07-26 03:57:20 +0900

    Add files via upload

--- js/baseScript.js
+++ js/baseScript.js
@@ -25,24 +25,20 @@ document.addEventListener('scroll', function () {
 });
 
 
-// 카카오 지도 API는 로딩이 끝난 뒤에 실행해야 합니다.
 window.addEventListener('load', function() {
-    var container = document.getElementById('map'); // 지도 표시할 div
+    var container = document.getElementById('map');
     var options = {
-      center: new kakao.maps.LatLng(37.566826, 126.9786567), // 중심 좌표
-      level: 3 // 확대 레벨
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //여기부분이 좌표임임
+      level: 3
     };
   
     // 지도 생성
     var map = new kakao.maps.Map(container, options);
   
     // 마커 생성
-    var markerPosition = new kakao.maps.LatLng(37.566826, 126.9786567);
     var marker = new kakao.maps.Marker({
-      position: markerPosition
+      position: new kakao.maps.LatLng(33.450701, 126.570667)
     });
-  
-    // 지도에 마커 표시
     marker.setMap(map);
 });
   
